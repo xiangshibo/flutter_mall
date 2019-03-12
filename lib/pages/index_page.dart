83b43989c72b5a8aff73c4c21e9fedb8252ce0e6
xiangshibo <xiangshibo@ligotop.com>
2019-03-12 16:53:14 +0800

    second commit

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatelessWidget {
+  final Widget child;
+
+  IndexPage({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('百姓生活+')),
+      body: Center(
+        child: Text('百姓生活+'),
+      ),
+    );
+  }
+}
