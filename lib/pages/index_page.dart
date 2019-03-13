19de32f16be856e71edd173be4248e3a7bcb3cbe
xiangshibo <xiangshibo@ligotop.com>
2019-03-13 09:29:54 +0800

    底部导航

--- lib/pages/index_page.dart
+++ lib/pages/index_page.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-class IndexPage extends StatelessWidget {
+class IndexPage extends StatefulWidget {
   final Widget child;
 
   IndexPage({Key key, this.child}) : super(key: key);
 
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      title: Text('首页'),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('百姓生活+')),
-      body: Center(
-        child: Text('百姓生活+'),
-      ),
+    return Container(
+       child: widget.child,
     );
   }
 }
